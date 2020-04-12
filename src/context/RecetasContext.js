311daf06297d489d8b2640997e35620bce18d04a
Junior Veli <junior_d27@hotmail.com>
2020-04-12 11:05:36 -0500

    proyecto bebidas alchlicas listar

---
+++ src/context/RecetasContext.js
@@ -0,0 +1,51 @@
+import React,{useState,createContext,useEffect} from 'react';
+import axios from 'axios';
+export const RecetasContext = createContext();
+
+
+
+const RecetasProvider = (props) => {
+
+    const [recetas, guardarRecetas] = useState([]);
+    const [busqueda, buscarRecetas] = useState({
+        nombre:'',
+        categoria:''
+    });
+
+    const [consultar,guardarConsultar] = useState(false);
+
+    const {nombre, categoria} = busqueda;
+
+    useEffect(() =>{
+
+        if(consultar){
+
+            const obtenerRecetas = async () =>{
+                const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${nombre}&c=${categoria}`;
+                
+
+                const resultado = await axios.get(url);
+
+                //console.log(resultado.data.drinks);
+                guardarRecetas(resultado.data.drinks);
+        }
+
+        obtenerRecetas();
+
+    }
+    }, [busqueda]);
+    // se pone un value para hacer disponible algunos valores
+    return ( 
+        <RecetasContext.Provider
+        value={{
+            recetas,
+            buscarRecetas,
+            guardarConsultar
+        }}
+        >
+            {props.children}
+        </RecetasContext.Provider>
+     );
+}
+ 
+export default RecetasProvider;
